768ea7b28198656151ac7d11b1fbf266c8f38433
Alejandro López Labajo <mainusone1@gmail.com>
2024-03-13 12:30:51 +0100

    Add project files.

---
+++ NetCoreSeguridadPersonalizada/Controllers/ManagedController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace NetCoreSeguridadPersonalizada.Controllers
+{
+    public class ManagedController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login
+            (string username, string password)
+        {
+            if (username.ToLower() == "admin"
+                && password.ToLower() == "admin")
+            {
+                // Aunque nosotros no lo veamos,
+                // por seguridad Session usa cookies
+                // Debemos crear una identidad para el usuario
+                // basada en cookies de autorización e indicar
+                // que nuestro user tendrá name y role
+                ClaimsIdentity identity =
+                    new ClaimsIdentity(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        ClaimTypes.Name,
+                        ClaimTypes.Role
+                    );
+                // Los claims indican características del usuario
+                Claim claimsUserName =
+                    new Claim(ClaimTypes.Name, username);
+                Claim claimRole =
+                    new Claim(ClaimTypes.Role, "USUARIO");
+                identity.AddClaim(claimsUserName);
+                identity.AddClaim(claimRole);
+                // Creamo user principal que será el que
+                // estará dentro de Session
+                ClaimsPrincipal userPrincipal =
+                    new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync
+                    (
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        userPrincipal,
+                        new AuthenticationProperties
+                        {
+                            ExpiresUtc = DateTime.Now.AddMinutes(15)
+                        });
+                // Llevamos usuario a su perfil
+                return RedirectToAction("Perfil", "Usuarios");
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Credenciales incorrectas";
+                return View();
+            }
+        }
+    }
+}
---
+++ NetCoreSeguridadPersonalizada/Filters/AuthorizeUsersAttribute.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NetCoreSeguridadPersonalizada.Filters
+{
+    public class AuthorizeUsersAttribute
+        : AuthorizeAttribute, IAuthorizationFilter
+    {
+        // Este método impedirá acceder a las zonas
+        // que hayamos decorado
+        // Dicho filter debe validar si existimos
+        // en la app o no
+        // Si no estamos validados en nuestra App,
+        // nos llevará a Login
+        // Si estamos validados, no hacemos nada
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            // El usuario estará dentro de HttpContext
+            // y su propiedad User
+            // Ese usuario pertenece a la clase principal
+            // e Identity
+            // Mediante la identidad podemos saber el nombre
+            // del user y mediante el principal el role
+            var user = context.HttpContext.User;
+            // Preguntamos si el user ya está autentificado
+            if (!user.Identity.IsAuthenticated)
+            {
+                // Creamos la ruta a nuestra dirección
+                RouteValueDictionary rutaLogin =
+                    new RouteValueDictionary
+                    (
+                        new { controller = "Managed", action = "Login" }
+                    );
+                // Llevamos al usuario a login
+                context.Result =
+                    new RedirectToRouteResult(rutaLogin);
+            }
+        }
+    }
+}
